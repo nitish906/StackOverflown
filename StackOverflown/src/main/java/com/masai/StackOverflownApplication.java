ebcb97e19f1ce3513182040115325d8bfb734908
Nitish Kumar <103942404+nitish906@users.noreply.github.com>
2022-11-03 17:16:10 +0530

    Stack Overflown

---
+++ StackOverflown/src/main/java/com/masai/StackOverflownApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackOverflownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StackOverflownApplication.class, args);
+	}
+
+}
